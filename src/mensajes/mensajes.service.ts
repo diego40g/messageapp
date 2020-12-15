08814dac7d1c0a48c11c260a3f79c9564b04e2fb
diego40g <diegopaznaula@hotmail.com>
2020-12-14 22:43:18 -0500

    Make some functions in data base select, create, update and delete

---
+++ src/mensajes/mensajes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Mensaje } from './entities/mensaje.entity';
+import { Repository } from "typeorm";
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
+
+@Injectable()
+export class MensajesService {
+
+    constructor(
+        @InjectRepository(Mensaje)
+        private readonly mensajeRepository: Repository<Mensaje>,
+    ){}
+
+    async getAll(){
+        return await this.mensajeRepository.find();
+    }
+
+    async createMensaje(mensajeNuevo: CreateMensajeDto){
+        const nuevo = new Mensaje
+        nuevo.mensaje = mensajeNuevo.mensjae;
+        nuevo.nick = mensajeNuevo.nick;
+        
+        return await this.mensajeRepository.save(nuevo);
+    }
+
+    async updateMensaje(idMensaje: number, mensajeActualizar: CreateMensajeDto){
+        const mensajeUpdate = await this.mensajeRepository.findOne(idMensaje)
+        mensajeUpdate.nick = mensajeActualizar.nick;
+        mensajeUpdate.mensaje = mensajeActualizar.mensjae
+
+        return await this.mensajeRepository.save(mensajeUpdate)
+    }
+
+    async deleteMensaje(idMensaje: number){
+        return await this.mensajeRepository.delete(idMensaje)
+    }
+}
